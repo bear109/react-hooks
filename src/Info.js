043f00973fddb237bb64fd0dae047d2a0675d962
bear109 <jokerbear109@gmail.com>
2021-07-09 20:48:40 +0900

    ch8-Hooks

---
+++ src/Info.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useReducer, useState } from 'react';
+import useInputs from './useInputs';
+
+const Info = () => {
+  const [state, dispatch] = useInputs({
+    name: '',
+    nickname: '',
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChange} />
+        <input value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름: </b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
